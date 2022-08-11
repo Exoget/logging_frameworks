a0954a6a3f3b1c3431704db7a294944acddf4260
Exoget <ferchichi.feres@gmail.com>
2022-08-11 22:53:57 +0200

    premier exemple logback

--- src/main/java/org/example/HomeResource.java
+++ src/main/java/org/example/HomeResource.java
@@ -8,10 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeResource {
 
+    // Logger SLF4j facade
     Logger logger = LoggerFactory.getLogger(HomeResource.class);
 
     @RequestMapping("/")
     public String home(){
+
+        // par défaut, c'est le framework logback qui est utilisé, configuré avec le fichier application.properties
         logger.error("Error happened ! ");
         logger.trace("TRACE happened ! ");
         return "Hello from controller !";
